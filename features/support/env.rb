1b5acb5feb2cc4f2ec4caa7a68e7bce8997a3165
Jocimar Daniel <jocimardaniel@gmail.com>
2021-04-27 10:38:54 -0300

    nova config para os webdrivers

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,16 @@
-require 'capybara'
-require 'capybara/cucumber'
-require 'selenium-webdriver'
+require "capybara"
+require "capybara/cucumber"
+require "webdrivers"
+require "faker"
+require "selenium-webdriver"
 
-Capybara.run_server = false
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.run_server = false
+end
+
+#Capybara.run_server = false
 #Using Mozila Firefox
-#Capybara.default_driver = :selenium 
+#Capybara.default_driver = :selenium
 #Using Google Chrome
-Capybara.default_driver = :selenium_chrome 
+#Capybara.default_driver = :selenium_chrome
